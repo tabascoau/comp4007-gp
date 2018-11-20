f115249286f0bb35a24ba72057a79ff2d2cd395b
tabascoau <43807516+tabascoau@users.noreply.github.com>
2018-11-20 17:04:32 +0800

    First commit and added connection to client and server

---
+++ src/AppKickstarter/AppKickstarter.java
@@ -0,0 +1,223 @@
+package AppKickstarter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
+import java.util.logging.Logger;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.Hashtable;
+
+import AppKickstarter.timer.Timer;
+import AppKickstarter.misc.*;
+import AppKickstarter.myThreads.ThreadA;
+import AppKickstarter.myThreads.ThreadB;
+
+
+//======================================================================
+// AppKickstarter
+public class AppKickstarter {
+    private String cfgFName = null;
+    private Properties cfgProps = null;
+    private Hashtable<String, AppThread> appThreads = null;
+    private String id = null;
+    private Logger log = null;
+    private ConsoleHandler logConHd = null;
+    private FileHandler logFileHd = null;
+    private Timer timer = null;
+    private ThreadA threadA1, threadA2;
+    private ThreadB threadB;
+
+
+    //------------------------------------------------------------
+    // main
+    public static void main(String [] args) {
+	AppKickstarter appKickstarter = new AppKickstarter("AppKickstarter", "etc/MyApp.cfg");
+	appKickstarter.startApp();
+	try {
+	    Thread.sleep(30 * 1000);
+	} catch (Exception e) {}
+	appKickstarter.stopApp();
+    } // main
+
+
+    //------------------------------------------------------------
+    // AppKickstarter
+    private AppKickstarter(String id) {
+	this(id, "etc/MyApp.cfg");
+    } // AppKickstarter
+
+
+    //------------------------------------------------------------
+    // AppKickstarter
+    private AppKickstarter(String id, String cfgFName) {
+	this(id, cfgFName, false);
+    } // AppKickstarter
+
+
+    //------------------------------------------------------------
+    // AppKickstarter
+    private AppKickstarter(String id, String cfgFName, boolean append) {
+	this.id = id;
+	this.cfgFName = cfgFName;
+	logConHd = null;
+	logFileHd = null;
+	id = getClass().getName();
+
+	// set my thread name
+	Thread.currentThread().setName(this.id);
+
+	// read system config from property file
+	try {
+	    cfgProps = new Properties();
+	    FileInputStream in = new FileInputStream(cfgFName);
+	    cfgProps.load(in);
+	    in.close();
+	    logConHd = new ConsoleHandler();
+	    logConHd.setFormatter(new LogFormatter());
+	    logFileHd = new FileHandler("etc/" + id + ".log", append);
+	    logFileHd.setFormatter(new LogFormatter());
+	} catch (FileNotFoundException e) {
+	    System.out.println("Failed to open config file ("+cfgFName+").");
+	    System.exit(-1);
+	} catch (IOException e) {
+	    System.out.println("Error reading config file ("+cfgFName+").");
+	    System.exit(-1);
+	}
+
+	// get and configure logger
+	log = Logger.getLogger(id);
+	log.addHandler(logConHd);
+	log.addHandler(logFileHd);
+	log.setUseParentHandlers(false);
+	log.setLevel(Level.FINER);
+	logConHd.setLevel(Level.INFO);
+	logFileHd.setLevel(Level.INFO);
+	appThreads = new Hashtable<String, AppThread>();
+    } // AppKickstarter
+
+
+    //------------------------------------------------------------
+    // startApp
+    private void startApp() {
+	// start our application
+	log.info("");
+	log.info("");
+	log.info("============================================================");
+	log.info(id + ": Application Starting...");
+
+	int port=54321;
+	try{
+		Thread t=new GreetingServer(port);
+		t.start();
+	}catch (IOException e){
+		e.printStackTrace();
+	}
+
+
+	// create threads
+	timer = new Timer("timer", this);
+	threadA1 = new ThreadA("ThreadA1", this);
+	threadA2 = new ThreadA("ThreadA2", this);
+	threadB  = new ThreadB("ThreadB",  this);
+
+	// start threads
+	new Thread(timer).start();
+	new Thread(threadA1).start();
+	new Thread(threadA2).start();
+	new Thread(threadB).start();
+    } // startApp
+
+
+    //------------------------------------------------------------
+    // stopApp
+    private void stopApp() {
+	log.info("");
+	log.info("");
+	log.info("============================================================");
+	log.info(id + ": Application Stopping...");
+	threadA1.getMBox().send(new Msg(id, null, Msg.Type.Terminate, "Terminate now!"));
+	threadA2.getMBox().send(new Msg(id, null, Msg.Type.Terminate, "Terminate now!"));
+	threadB.getMBox().send(new Msg(id, null, Msg.Type.Terminate, "Terminate now!"));
+	timer.getMBox().send(new Msg(id, null, Msg.Type.Terminate, "Terminate now!"));
+    } // stopApp
+
+
+    //------------------------------------------------------------
+    // regThread
+    public void regThread(AppThread appThread) {
+	log.fine(id + ": registering " + appThread.getID());
+	synchronized (appThreads) { appThreads.put(appThread.getID(), appThread); }
+    } // regThread
+
+
+    //------------------------------------------------------------
+    // unregThread
+    public void unregThread(AppThread appThread) {
+	log.fine(id + ": unregistering " + appThread.getID());
+	synchronized (appThreads) { appThreads.remove(appThread.getID()); }
+    } // unregThread
+
+
+    //------------------------------------------------------------
+    // getThread
+    public AppThread getThread(String id) {
+	synchronized (appThreads) { return appThreads.get(id); }
+    } // getThread
+
+
+    //------------------------------------------------------------
+    // getLogger
+    public Logger getLogger() {
+	return log;
+    } // getLogger
+
+
+    //------------------------------------------------------------
+    // getLogConHd
+    public ConsoleHandler getLogConHd() {
+	return logConHd;
+    }
+    // getLogConHd
+
+
+    //------------------------------------------------------------
+    // getLogFileHd
+    public FileHandler getLogFileHd() {
+	return logFileHd;
+    } // getLogFileHd
+
+
+    //------------------------------------------------------------
+    // getProperty
+    public String getProperty(String property) {
+	String s = cfgProps.getProperty(property);
+
+	if (s == null) {
+	    log.severe(id + ": getProperty(" + property + ") failed.  Check the config file (" + cfgFName + ")!");
+	}
+	return s;
+    } // getProperty
+
+
+    //------------------------------------------------------------
+    // getSimulationTime (in seconds)
+    public long getSimulationTime() {
+	return timer.getSimulationTime();
+    } // getSimulationTime
+
+
+    //------------------------------------------------------------
+    // getSimulationTimeStr
+    public String getSimulationTimeStr() {
+	long t = timer.getSimulationTime();
+	int s = (int) t % 60;
+	int m = (int) (t/60) % 60;
+	int h = (int) (t/3600) % 60;
+
+	return String.format("%02d:%02d:%02d", h, m, s);
+    } // getSimulationTimeStr
+} // AppKickstarter
---
+++ src/AppKickstarter/misc/GreetingServer.java
@@ -0,0 +1,43 @@
+package AppKickstarter.misc;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class GreetingServer extends Thread {
+    private ServerSocket serverSocket;
+
+    public GreetingServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Waiting for client on port " +
+                        serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+
+                System.out.println("Just connected to " + server.getLocalSocketAddress());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+                        + "\nGoodbye!");
+                server.close();
+
+            } catch (SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
